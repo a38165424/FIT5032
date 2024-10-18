d9355bea1d97be7d947be4dc9bb5290e9d6341e6
jzha0477 <jzha0477@student.monash.edu>
2024-10-18 12:09:21 +1100

    Configure GitHub Actions for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-//import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +11,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: process.env.NODE_ENV === 'production' ? '/FIT5032/' : '/'  // 确保此处的仓库名称正确
 })
